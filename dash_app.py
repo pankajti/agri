11ce5963ae8b1e6acc3c5f21ff8cafc228b3ff7f
Pankaj Kumar <pankaj.tiwari2@gmail.com>
2025-08-13 03:57:30 +0800

    changing application layout

--- dash_app.py
+++ dash_app.py
@@ -3,5 +3,5 @@ from agri.gui.usda_analytics.app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=False)
+if __name__ == '__main__':
+    app.run(debug=False,host='0.0.0.0', port=8053)
